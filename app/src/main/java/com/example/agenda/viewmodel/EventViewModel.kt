3ea676a226bed1baa8f4e071111824020073e0ee
hugobarbosa <hugodu38@icloud.com>
2023-07-24 18:50:17 +0200

    Switch dispatcher to IO because dispatchers.main is user_main_thread

--- app/src/main/java/com/example/agenda/viewmodel/EventViewModel.kt
+++ app/src/main/java/com/example/agenda/viewmodel/EventViewModel.kt
@@ -4,12 +4,13 @@ import androidx.lifecycle.viewModelScope
 import com.example.agenda.models.Event
 import com.example.agenda.repository.EventRepository
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.Dispatchers
 
 class EventViewModel(private val repository: EventRepository) : ViewModel() {
 
     val allEvents = repository.allEvents
 
-    fun insert(event: Event) = viewModelScope.launch {
+    fun insert(event: Event) = viewModelScope.launch(Dispatchers.IO) {
         repository.insert(event)
     }
 
